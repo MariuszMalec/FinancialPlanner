0690fb1428eb8e6d659343bae23471fa9244705a
MariuszMalec <mario106c@gmail.com>
2023-03-23 21:24:18 +0100

    add console project

--- FinancialPlanner.Logic/Services/LoadDataService.cs
+++ FinancialPlanner.Logic/Services/LoadDataService.cs
@@ -7,10 +7,17 @@ namespace FinancialPlanner.Logic.Services
     {
         public static List<User> ReadUserFile()
         {
-            string fileName = @"Source\users.json";
-            string jsonString = File.ReadAllText(fileName);
-            List<User> userData = JsonConvert.DeserializeObject<List<User>>(jsonString);
-            return userData;
+            string fileName = @"Source/users.json";
+            string getDir = Directory.GetCurrentDirectory();
+            fileName = Path.Combine(getDir, fileName);
+            Console.WriteLine($"{fileName}");
+            if (File.Exists(fileName))
+            {
+                string jsonString = File.ReadAllText(fileName);
+                List<User>? userData = JsonConvert.DeserializeObject<List<User>>(jsonString);
+                return userData;
+            }
+            return new List<User>(){};
         }
     }
 }
