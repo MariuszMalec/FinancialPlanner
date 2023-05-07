7e9527684ea6475bbe27412651ea9603c55a9886
Mariusz Malec <mario106c@gmail.com>
2023-05-07 11:08:43 +0200

    fix view monthly balance in console

--- FinancialPlanner.ConsoleApp/Service/MonthlyBalanceViewer.cs
+++ FinancialPlanner.ConsoleApp/Service/MonthlyBalanceViewer.cs
@@ -8,7 +8,22 @@ namespace FinancialPlanner.ConsoleApp.Service
         {
             if (balance.Count() > 0)
             {
-                balance.ForEach(x => Console.WriteLine($"{x.Month.ToString("MMMM")} {x.Income} {x.Expenses} {x.Income - x.Expenses} {x.Differrence}"));
+                var textPaddingWidth = 14;
+                var paddingChar = ' ';
+                var numberOfCollumn = 5;
+                Console.WriteLine(("").PadRight(textPaddingWidth * numberOfCollumn, '='));
+                Console.WriteLine($"|{"Month".PadRight(textPaddingWidth, paddingChar)}" +
+                                  $"|{"Income".PadRight(textPaddingWidth, paddingChar)}" +
+                                  $"|{"Expanses".PadRight(textPaddingWidth, paddingChar)}" +
+                                  $"|{"Income-Outcome".PadRight(textPaddingWidth, paddingChar)}" +
+                                  $"|{"Balance".PadRight(textPaddingWidth, paddingChar)}");
+                Console.WriteLine(("").PadRight(textPaddingWidth * numberOfCollumn, '='));
+
+                balance.ForEach(x => Console.WriteLine($"|{x.Month.ToString("MMMM").ToString().PadRight(textPaddingWidth, paddingChar)}" +
+                                                       $"|{x.Income.ToString().PadRight(textPaddingWidth, paddingChar)}" +
+                                                       $"|{x.Expenses.ToString().PadRight(textPaddingWidth, paddingChar)}" +
+                                                       $"|{(x.Income - x.Expenses).ToString().PadRight(textPaddingWidth, paddingChar)}" +
+                                                       $"|{x.Balance.ToString().PadRight(textPaddingWidth, paddingChar)}"));
             }
         }
     }
