dbffe6abf78790527688b683a0597c01476c94ca
MariuszMalec <mario106c@gmail.com>
2023-11-01 23:04:36 +0100

    add _headpanel

---
+++ FinancialPlanner.WebMvc/Views/Shared/_HeadPanel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinancialPlanner.WebMvc.Views.Shared
+{
+    public class _HeadPanelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
