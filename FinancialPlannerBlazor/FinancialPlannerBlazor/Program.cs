61bb2834a786181e5d748591c3781717d727aa8f
MariuszMalec <mario106c@gmail.com>
2024-06-21 21:04:16 +0200

    add transactions to blazor

--- FinancialPlannerBlazor/FinancialPlannerBlazor/Program.cs
+++ FinancialPlannerBlazor/FinancialPlannerBlazor/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddDbContext<ApplicationDbContext, PostgresDbContext>();
 
 builder.Services.AddBlazorBootstrap();
 builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddTransient<ITransactionService, TransactionService>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 var app = builder.Build();
